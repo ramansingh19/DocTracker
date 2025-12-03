c71d83c24dbf31c9c71f6584cde448dffffd5ef3
ramansingh19 <raman.01kumar1@gmail.com>
2025-12-04 01:17:51 +0530

    s

--- backend/src/routes/locationRoutes.js
+++ backend/src/routes/locationRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { updateLocation } from "../controllers/locationController.js";
-import authMiddleware from "../middleware/auth.js";
+import { updateLocation } from "../controllers/locationcontroller.js";
+import { authenticate } from "../middleware/auth.js";
+
 
 const router = express.Router();
 
-router.post("/update", authMiddleware, updateLocation);
+router.post("/update", authenticate, updateLocation);
 
 export default router;
